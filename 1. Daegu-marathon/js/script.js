e84520282fd0e7310bb0ac1f26dbbeb31de91d5f
soyoungpark-tech <ps.young149@gmail.com>
2021-06-15 01:22:02 +0900

    css, js: adding effect
    1 대구마라톤 홈페이지 nav bar js 효과 적용, 컨텐츠, 푸터 추가

--- 1. Daegu-marathon/js/script.js
+++ 1. Daegu-marathon/js/script.js
@@ -1,29 +1,40 @@
 'use strict';
 
 $(function(){
-        /* ================= navbar ===================== */
-        // var $navItem = $('.nav__item');
-        // $navItem.click(function(e){
-        //     e.preventDefault();
-        //     var link = $(this).children('a').attr('data-link');
-        //     var $target = $(link);
-        //     var distance = $target.offset().top;
-        //     $('html, body').animate({scrollTop: distance});
-        //     $('.nav__container').removeClass('visible');
-        //     $(this).siblings().removeClass('active');
-        //     $(this).addClass('active');
-        // })
     
-        // /* nav toggle */
-        // $('#navToggle, .nav__close').click(function(){
-        //     $('.nav__container').toggleClass('visible');
-        // })
+        /* ================= navbar ===================== */
+        var $header = $('#header');
+        var $navItem = $('.nav__item');
+
+        $(window).scroll(function(){
+            var scrollTop = $(window).scrollTop();
+            var headerHeight = $('#header').outerHeight();
+            var homeHeight = $('#home').outerHeight();
+    
+            if( scrollTop > headerHeight ){
+                $header.addClass('fixed');
+                $navItem.children().addClass('fixed');
+                $navItem.children().addClass('active');
+            }else{
+                $header.removeClass('fixed');
+                $navItem.children().removeClass('fixed');
+            }
+    
+            // aboutSection을 지나치면 보여라
+            // scrollTop 값이 about section 높이보다 커지면
+            // if( scrollTop > aboutHeight ){
+            //     $arrowUp.addClass('visible');
+            // }else{
+            //     $arrowUp.removeClass('visible');
+            // }        
+    
+        })
 
         /* nav submenu */
         var $navItem = $('.nav__item'),
             $navMenu = $('.nav__link'),
             $subMenu = $('.nav__submenu');
-        console.log($subMenu);
+        // console.log($subMenu);
         $subMenu.hide();
 
         $navItem.mouseenter(function(){
